1021185a6ca86126edb790714b3bb3792d2ca922
NineDisc <queue4423@gmail.com>
2024-03-20 16:01:13 +0900

    UIのみ実装

--- app/src/main/java/com/ninedisc/todoapp/feature/TodoScreen.kt
+++ app/src/main/java/com/ninedisc/todoapp/feature/TodoScreen.kt
@@ -1,13 +1,89 @@
 package com.ninedisc.todoapp.feature
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun TodoScreen(){
-
+fun TodoScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .background(Color.White)
+    ) {
+        LazyColumn(
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(dummyTodoList) {
+                TodoItem(todoTitle = it)
+            }
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.padding(bottom = 16.dp, end = 16.dp)
+            ) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+            }
+        }
+    }
 }
 
 @Composable
-fun TodoItem(){
-
+fun TodoItem(todoTitle: String) {
+    Card {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+        ) {
+            Button(onClick = { /*Todo Remove TodoItem */ }) {
+                Text(text = "COMPLETE")
+            }
+            Text(text = todoTitle)
+        }
+    }
 }
+
+@Preview
+@Composable
+fun TodoItemPreview() {
+    TodoItem("TodoPreview")
+}
+
+@Preview
+@Composable
+fun TodoScreenPreview() {
+    TodoScreen()
+}
+
+private val dummyTodoList = mutableListOf("Item1", "Item2", "Item3", "Item4")
--- app/src/main/java/com/ninedisc/todoapp/ui/TodoApp.kt
+++ app/src/main/java/com/ninedisc/todoapp/ui/TodoApp.kt
@@ -5,12 +5,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.ninedisc.todoapp.feature.TodoScreen
 
 @Composable
 fun TodoApp() {
     Scaffold {
         Row(modifier = Modifier.padding(it)) {
-
+            TodoScreen()
         }
     }
 }
